4d4d8004c1bd13bf24803c71b5fa4b0e0f6ddce9
gravyytron <34137021+gravyytron@users.noreply.github.com>
2018-04-14 18:09:18 +0200

    Initial commit

---
+++ client/Client.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.TimeUnit;
+
+public class Client extends Actor
+{
+    public BufferedReader in;
+    public BufferedWriter out;
+    public Socket socket;
+    public boolean connected = false;
+    
+    public Client() {
+    }
+    
+    public void act() 
+    {
+            //if (!this.connected) {this.connect();}
+            //this.checkIncomingMessages();
+            this.send("test");
+    }    
+    
+    public void connect() {
+        try {
+            this.socket = new Socket("localhost", 1223);
+            this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+            this.out = bw;
+            this.connected = true;
+            System.out.println(this + ": connected");
+        } catch (IOException e) {e.printStackTrace();}
+    }
+    
+    public void send(String data) {
+       try {
+           //this.out.writeBytes(data);
+           this.out.write(data + "\n");
+           this.out.flush();
+           System.out.println(this + ": [out] " + data);
+       } catch (IOException e) {e.printStackTrace();}
+    }
+    
+    public void checkIncomingMessages() {
+        String data;
+        try {
+            if ((data = this.in.readLine()) != null) {
+                System.out.println(this + ": [in] " + data);
+            }
+        } catch (IOException e) {e.printStackTrace();}
+    }
+    
+}
---
+++ client/CrabWorld.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
+
+public class CrabWorld extends World
+{
+    public CrabWorld() 
+    {
+        super(1200, 600, 1);
+        prepare();
+    }
+
+    public void prepare(){
+        Client client = new Client();
+        addObject(client,113,93);
+    }
+}
